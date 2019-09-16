265c530c4c5c35b62f73096be3a24bc95de94fa6
oscarfont <oscar.font01@estudiant.upf.edu>
2019-09-16 14:25:08 +0200

    first commit

---
+++ src/main/java/com/idealista/infrastructure/api/AdsController.java
@@ -0,0 +1,38 @@
+package com.idealista.infrastructure.api;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdsController {
+
+    //TODO añade url del endpoint
+    @GetMapping("/ads/quality")
+    //public ResponseEntity<List<QualityAd>> qualityListing() {
+    //    return ResponseEntity.notFound().build();
+    public String qualityListing(){
+        //TODO rellena el cuerpo del método
+        return "List of ads for the quality management.\n";
+    }
+
+    //TODO añade url del endpoint
+    @GetMapping("/ads/public")
+    //public ResponseEntity<List<PublicAd>> publicListing() {
+        //return ResponseEntity.notFound().build();
+    public String publicListing() {
+        //TODO rellena el cuerpo del método
+        return "List of public ads.\n";
+    }
+
+    //TODO añade url del endpoint
+    @GetMapping("/ads/score")
+    //public ResponseEntity<Void> calculateScore() {
+    // return ResponseEntity.notFound().build();
+    public String calculateScore() {
+        //TODO rellena el cuerpo del método
+        return "Ads scores computed!\n";
+    }
+}
